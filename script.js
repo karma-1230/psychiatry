bf91d84c173c7c322119e3da4eee914317774276
K A R M A <129971569+karma-1230@users.noreply.github.com>
2024-10-29 22:41:46 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const mobileMenuButton = document.getElementById('mobile-menu-button');
-  const mobileMenu = document.getElementById('mobile-menu');
-
-  mobileMenuButton.addEventListener('click', () => {
-    mobileMenu.classList.toggle('hidden');
-  });
-
-  // Close mobile menu when a link is clicked
-  const mobileMenuLinks = mobileMenu.querySelectorAll('a');
-  mobileMenuLinks.forEach(link => {
-    link.addEventListener('click', () => {
-      mobileMenu.classList.add('hidden');
-    });
-  });
-
+  
   // Hero Section Animations
   gsap.to('.animated-text', {
     opacity: 1,
